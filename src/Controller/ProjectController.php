b288d6a4a96c14faf9d6e0afbf2648ac3ba54841
keuss le mojo <julio.nathan47@gmail.com>
2024-12-04 10:00:46 +0100

    project vue et tout laaa

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Project;
+use App\Form\ProjectType;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProjectController extends AbstractController
+{
+    #[Route('/project', name: 'app_project')]
+    public function index(ProjectRepository $projectRepository): Response
+    {
+        // Récupère toutes les priorités à partir du repository
+        $projects = $projectRepository->findAll();
+
+        // Rend la vue Twig avec les priorités récupérées
+        return $this->render('project/index.html.twig', [
+            'projects' => $projects,
+        ]);
+    }
+
+    #[Route('/project/create', name: 'create_project')]
+    public function create(EntityManagerInterface $entityManager, Request $request): Response {
+        $project = new Project();
+        $form = $this->createForm(ProjectType::class, $project);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $entityManager->persist($project);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_project');
+        }
+
+        $formView = $form->createView();
+        return $this->render('project/create_project.html.twig', compact('formView'));
+    }
+}
---
+++ src/Form/ProjectType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Project;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProjectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+
+            ->add('name', TextType::class)
+            ->add('description', TextAreaType::class)
+            ->add('image', FileType::class)
+            ->add('Enregistrer', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Project::class,
+        ]);
+    }
+}
